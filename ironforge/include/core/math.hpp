9c17a8d871d01431eb5bfc5a74d46061a867cc83
m1nuz <xminuz@gmail.com>
2019-03-06 11:59:37 +0200

    Update build with cmake packages

--- ironforge/include/core/math.hpp
+++ ironforge/include/core/math.hpp
@@ -4,6 +4,7 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow"
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/string_cast.hpp>
