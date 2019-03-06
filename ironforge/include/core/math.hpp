6203a087f0a1a5f16f5fdba6dcc75c6ca01f6e11
minuz <xminuz@gmail.com>
2019-03-06 23:19:12 +0200

    remove glm from /lib

--- ironforge/include/core/math.hpp
+++ ironforge/include/core/math.hpp
@@ -16,15 +16,8 @@
 
 namespace glm {
 
-    static inline void to_json( nlohmann::json &j, const glm::vec3 &p ) {
-        j.array( {p.x, p.y, p.z} );
-    }
+    void to_json( nlohmann::json &j, const glm::vec3 &p );
 
-    static inline void from_json( const nlohmann::json &j, glm::vec3 &p ) {
-        int ix = 0;
-        for ( auto it = j.begin( ); it != j.end( ); ++it, ix++ ) {
-            p[ix] = *it;
-        }
-    }
+    void from_json( const nlohmann::json &j, glm::vec3 &p );
 
 } // namespace glm
