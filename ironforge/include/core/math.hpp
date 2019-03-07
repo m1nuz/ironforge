043df07856cd083ebe43a22f8c29f764c9a4d9dc
minuz <xminuz@gmail.com>
2019-03-07 06:43:49 +0200

    Update build for glm

--- ironforge/include/core/math.hpp
+++ ironforge/include/core/math.hpp
@@ -2,22 +2,28 @@
 
 #define randf() (rand() / (float)RAND_MAX)
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wshadow"
 #define GLM_ENABLE_EXPERIMENTAL
 #define GLM_FORCE_RADIANS
+#define GLM_FORCE_SILENT_WARNINGS
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/string_cast.hpp>
 #include <glm/gtc/quaternion.hpp>
-#pragma GCC diagnostic pop
 
 #include <core/json.hpp>
 
 namespace glm {
 
-    void to_json( nlohmann::json &j, const glm::vec3 &p );
+    inline void to_json( nlohmann::json &j, const glm::vec3 &p ) {
+        j.array( {p.x, p.y, p.z} );
+    }
 
-    void from_json( const nlohmann::json &j, glm::vec3 &p );
+    inline void from_json( const nlohmann::json &j, glm::vec3 &p ) {
+        int ix = 0;
+        for ( auto it = j.begin( ); it != j.end( ); ++it, ix++ ) {
+            p[ix] = *it;
+        }
+    }
 
 } // namespace glm
--- ironforge/src/core/math.cpp
+++ ironforge/src/core/math.cpp
@@ -1,16 +1,1 @@
 #include <core/math.hpp>
-
-namespace glm {
-
-    void to_json( nlohmann::json &j, const glm::vec3 &p ) {
-        j.array( {p.x, p.y, p.z} );
-    }
-
-    void from_json( const nlohmann::json &j, glm::vec3 &p ) {
-        int ix = 0;
-        for ( auto it = j.begin( ); it != j.end( ); ++it, ix++ ) {
-            p[ix] = *it;
-        }
-    }
-
-} // namespace glm
